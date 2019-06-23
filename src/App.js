0ed24ac646ba59605aec1631b9b5722306e2a64c
chirayu.bhatnagar <chirayu.bhatnagar@getzephyr.com>
2019-06-23 15:49:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 
-import Calendar from "./hoc/Calendar";
+import Calendar from "./calendar/Calendar";
 import Month from "./containers/Month";
 
 export default class App extends React.Component {
